69787ff364c80ff1204250af08de26c8ec5a3a8f
Sofía Guzmán <sofipguz@gmail.com>
2025-10-16 18:35:22 -0600

    initial project

---
+++ src/test/java/com/sofipguz/Challenge5/Challenge5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofipguz.Challenge5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
